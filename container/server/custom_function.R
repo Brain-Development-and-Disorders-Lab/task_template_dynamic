c3a6650aa69ac86ff8bce30fc9fea51b77a80e52
Henry Burgess <henryjburg@gmail.com>
2023-01-10 11:39:59 -0600

    Update custom_function.R

--- container/server/custom_function.R
+++ container/server/custom_function.R
@@ -2,8 +2,5 @@ custom_function <- function(parameters) {
   # The content of this function should parse the received parameters
   # and execute whatever computations are required
 
-  # Implement function here...
-
-  # This function should return a list() of parameters
   parameters
 }
